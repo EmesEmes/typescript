4e3e37ced64a6129c265e157a9db5ce9556a17ec
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-09 09:35:12 -0500

    tipo booleanos

---
+++ bases/tipos/boolean.ts
@@ -0,0 +1,9 @@
+(() => {
+    let isSuperman: boolean = true;
+    let isBatman: boolean = false;
+    
+    isSuperman = isBatman ? true : false;
+    isSuperman = isBatman ? false : true;
+    
+    console.log({ isSuperman });
+})();
