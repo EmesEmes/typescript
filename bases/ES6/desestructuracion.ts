25218f3c834add013e983082cdeee2a5fc93c151
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-13 17:28:22 -0500

    ES6/desestructuracion. ts agregado

---
+++ bases/ES6/desestructuracion.ts
@@ -0,0 +1,24 @@
+(() => {
+    type Avengers = {
+        nick: string;
+        ironman: string;
+        vision: string;
+        activo: boolean;
+        poder: number;
+    }
+    const avengers: Avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr.',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500.123456
+    }
+
+    const {poder, vision} = avengers;
+    console.log(poder.toFixed(2), vision.toUpperCase());
+
+    const printAvenger = ({vision, ...resto}: Avengers ) => {
+        console.log(vision, resto)
+    }
+    printAvenger(avengers)
+})();
