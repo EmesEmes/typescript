b2e8d97c5d05a2a530e2df92895d0714c9d70d87
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-13 17:15:24 -0500

    ES6/let-var.ts agregado

---
+++ bases/ES6/let-var.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    // console.log(a);
+    const NOMBRE = 'Emilio';
+    const getName = () => {
+        console.log('Desde funcion de flech');
+    };
+    // No se recomienda usar VAR en ES6 y se debería  usar const lo maximo posible a mennos que sepanamos que el valor de la variable a fuerza va a cambia
+})();
---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,11 @@
+(() => {
+    // console.log(a);
+    const NOMBRE: string = 'Emilio';
+
+    const getName = (): void => {
+        console.log('Desde funcion de flech')
+    }
+
+    // No se recomienda usar VAR en ES6 y se debería  usar const lo maximo posible a mennos que sepanamos que el valor de la variable a fuerza va a cambia
+    
+})();
