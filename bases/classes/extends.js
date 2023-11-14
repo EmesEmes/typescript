bbf846fd0a52ffb8abf534379772967051d2f0af
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-14 11:46:52 -0500

    classes/extends.ts agregado

---
+++ bases/classes/extends.js
@@ -0,0 +1,34 @@
+"use strict";
+(() => {
+    class Avenger {
+        constructor(name, realName) {
+            this.name = name;
+            this.realName = realName;
+            console.log("Constructor Avenger llamado");
+        }
+        getFullName() {
+            return `${this.name} ${this.realName}`;
+        }
+    }
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log('Constructor Xmen llamado');
+        }
+        get fullName() {
+            return `${this.name} - ${this.realName}`;
+        }
+        set FullName(name) {
+            this.name = name;
+        }
+        getFullNameDesdeXmen() {
+            console.log(super.getFullName());
+        }
+    }
+    const wolverine = new Xmen('wolverine', 'Logan', true);
+    console.log(wolverine);
+    wolverine.getFullNameDesdeXmen();
+    wolverine.FullName = 'Emilio';
+    console.log(wolverine.fullName);
+})();
