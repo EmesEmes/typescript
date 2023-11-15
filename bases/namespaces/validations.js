617f16fff3bff060685a65c599bd50aedf45f947
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-15 18:34:51 -0500

    namespaces/validations.ts agregado

---
+++ bases/namespaces/validations.js
@@ -0,0 +1,16 @@
+"use strict";
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        if (text.length > 3) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
+    };
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Emilio'));
