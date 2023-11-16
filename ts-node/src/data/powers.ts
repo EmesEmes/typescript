f0d6314a1761b5604b3eb8203a90dbeaa65e8e73
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-15 19:56:17 -0500

    proyecto ts-node agregado

---
+++ ts-node/src/data/powers.ts
@@ -0,0 +1,17 @@
+interface Power {
+    id: number;
+    desc: string;
+}
+
+const powers: Power[] = [
+    {
+        id: 1, 
+        desc: 'money' 
+    },
+    {
+        id: 2,
+        desc: 'Drugs'
+    }
+];
+
+export default powers;
---
+++ ts-node/src/index.ts
@@ -0,0 +1,12 @@
+import { Hero  } from "./classes/Hero";
+
+const ironman = new Hero('Ironman', 1, 35);
+
+console.log(ironman);
+
+console.log(ironman.power);
+
+
+
+
+
